11abcc3ca5ddb3d4db014d848a0ea28b86a92458
Jyotirmaya Maharana <146333462+jyotirmaya16@users.noreply.github.com>
2024-04-30 18:08:49 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -140,3 +140,24 @@ const sections = document.querySelectorAll('section[id]')
     })
   }
   window.addEventListener('scroll', scrollActive)
+
+
+// automatically count
+const counts = document.querySelectorAll('.count')
+const speed = 80
+
+counts.forEach((counter) => {
+    function upDate(){
+        const target = Number(counter.getAttribute('data-target'))
+        const count = Number(counter.innerText)
+        const inc = target / speed        
+        if(count < target){
+            counter.innerText = Math.floor(inc + count) 
+            setTimeout(upDate, 18)
+        }else{
+            counter.innerText = target+' %'
+        }
+    }
+    upDate()
+})
+
