03279eb6106f697e6236d9afca4443189a397591
Jyotirmaya Maharana <146333462+jyotirmaya16@users.noreply.github.com>
2024-05-04 12:07:45 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -160,3 +160,94 @@ counts.forEach((counter) => {
     }
     upDate()
 })
+
+// bloob anuimation code pane
+const MIN_SPEED = 1.5
+const MAX_SPEED = 2.5
+
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min
+}
+
+class Blob {
+  constructor(el) {
+    this.el = el
+    const boundingRect = this.el.getBoundingClientRect()
+    this.size = boundingRect.width
+    this.initialX = randomNumber(0, window.innerWidth - this.size)
+    this.initialY = randomNumber(0, window.innerHeight - this.size)
+    this.el.style.top = `${this.initialY}px`
+    this.el.style.left = `${this.initialX}px`
+    this.vx =
+      randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1)
+    this.vy =
+      randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1)
+    this.x = this.initialX
+    this.y = this.initialY
+  }
+
+  update() {
+    this.x += this.vx
+    this.y += this.vy
+    if (this.x >= window.innerWidth - this.size) {
+      this.x = window.innerWidth - this.size
+      this.vx *= -1
+    }
+    if (this.y >= window.innerHeight - this.size) {
+      this.y = window.innerHeight - this.size
+      this.vy *= -1
+    }
+    if (this.x <= 0) {
+      this.x = 0
+      this.vx *= -1
+    }
+    if (this.y <= 0) {
+      this.y = 0
+      this.vy *= -1
+    }
+  }
+
+  move() {
+    this.el.style.transform = `translate(${this.x - this.initialX}px, ${
+      this.y - this.initialY
+    }px)`
+  }
+}
+
+function initBlobs() {
+  const blobEls = document.querySelectorAll('.bouncing-blob')
+  const blobs = Array.from(blobEls).map((blobEl) => new Blob(blobEl))
+
+  function update() {
+    requestAnimationFrame(update)
+    blobs.forEach((blob) => {
+      blob.update()
+      blob.move()
+    })
+  }
+
+  requestAnimationFrame(update)
+}
+
+initBlobs()
+
+// fixed back to top button
+
+// Get the button
+var button = document.querySelector('.fixbc');
+
+// Function to toggle button visibility based on scroll position
+function toggleButton() {
+  const button = document.querySelector('.fixbc'); // Get the button
+  if (window.scrollY > 300 && window.scrollY < 3500) {
+    button.classList.add('show'); // Add show class to display the button
+  } else {
+    button.classList.remove('show'); // Remove show class to hide the button
+  }
+}
+
+// Add scroll event listener to toggle button visibility
+window.addEventListener('scroll', toggleButton);
+
+// Call the toggleButton function once on page load to handle initial state
+toggleButton();
